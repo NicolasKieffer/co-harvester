c56520e7ad602a9fe2d1850021a3402d10b81d41
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2020-02-19 16:24:06 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # co-harvester
-Harverster compatible with Conditor API
+
+Harverster compatible avec l'API Conditor. Il peut faire une simple requête ou effectuer un scroll (récupération de corpus)
+
+## Installation ##
+
+```bash
+npm install
+```
+
+## Prérequis ##
+
+Le token d'authentification est nécessaire pour pouvoir accèder à l'API Conditor.
+
+```bash
+# export variable
+export CONDITOR_TOKEN="myToken";
+# pass variable to process node
+env CONDITOR_TOKEN="myToken" node index.js
+node index.js --token="myToken"
+# set token into conditor query url
+node index.js  --query="http://api.conditor.fr/v1/records?q=%22*%22&access_token=myToken"
+```
+
+## Help ##
+
+```bash
+node index.js --help
+```
+
+### Liste des paramètres disponibles pour l'API conditor ###
+
+
+```bash
+--query="a valid conditor query url"
+```
+
+Consultez [la documentation de l'API](https://github.com/conditor-project/api) pour plus d'informations.
+
+### Liste des autres paramètres du harvester ###
+
+```bash
+--output --criteria --format
+```
+
+Le paramètre `output` permet de définir le chemin du fichier de sortie. Par défaut : `"./output.txt"`
+
+```bash
+--output="myPath" # Chemin du fichier de sortie.
+```
+
+Il est possible de formater les données en utilisant : `--format` et `--criteria`.
+Par défaut, le résultat brut de l'API est renvoyé par le harvester.
+
+Le paramètre `criteria` doit obligatoirement être [une des valeurs suivantes](https://github.com/conditor-project/api/blob/master/doc/recordFields.md)
+
+```bash
+--criteria="doi" # Critère utilisé pour regrouper les 'hits' entre eux (ex: doi, issue, halId, etc).
+```
+
+Le paramètre `format` permet de modifier la structure du résultat d'un scroll.
+
+```bash
+--format="object" # Résultat sous la forme d'objet (regroupé par 'criteria' de même valeur)
+--format="array" # Résultat sous la forme d'un tableau (regroupé par 'criteria' de même valeur)
+--format="list" # Résultat sous la forme d'une liste (dédoublonnée) de toutes les valeurs de 'criteria' (ex : liste des doi)
+```
+
+## Exemples ##
+
+Récupérer tous les documents ayant un doi (scroll) :
+
+```bash
+# localhost
+node index.js --query="http://localhost:63332/v1/records?q=%22doi:*%22&page_size=1000&includes=doi&scroll=1m" --scroll
+# serveur distant
+node index.js --query="https://api.conditor.fr/v1/records?q=%22doi:*%22&page_size=1000&includes=doi&scroll=1m" --token="myToken" --scroll
+```
+
+Récupérer la liste des doi (scroll) :
+
+```bash
+node index.js --query="http://localhost:63332/v1/records?q=%22doi:*%22&page_size=1000&includes=doi&scroll=1m" --format="list" --criteria="doi"
+```
+
+Récupérer le nombre de documents ayant un doi ([aggrégations](https://github.com/conditor-project/api/blob/master/doc/aggregations.md)) :
+
+```bash
+node index.js --query="http://localhost:63332/v1/records?q=%22doi:*%22&aggs=cardinality:doi.normalized&page_size=0"
+```
+
---
+++ index.js
@@ -0,0 +1,225 @@
+/*
+ * @prettier
+ */
+
+"use strict";
+
+const fetch = require("node-fetch"),
+  url = require("url"),
+  fs = require("fs"),
+  path = require("path"),
+  cliProgress = require("cli-progress"),
+  program = require("commander"),
+  colors = require("colors/safe");
+
+program
+  .requiredOption(
+    "--query <query>",
+    colors.yellow(colors.bold("required")) + "   conditor query"
+  )
+  .option(
+    "--token <token>",
+    colors.gray(colors.bold("optionnal")) + "  authentication token"
+  )
+  .option("--scroll", colors.gray(colors.bold("optionnal")) + "  scroll mode")
+  .option(
+    "--criteria <criteria>",
+
+    colors.gray(colors.bold("optionnal")) + "  property used to regroup results"
+  )
+  .option(
+    "--format <object|array|list>",
+
+    colors.gray(colors.bold("optionnal")) +
+      "  format used to modify structure of API results"
+  )
+  .option(
+    "--output <output>",
+    colors.gray(colors.bold("optionnal")) + "  output file path",
+    "./scroll.out"
+  )
+  .option("--quiet", colors.gray(colors.bold("optionnal")) + "  quiet mode")
+  .on("--help", function() {
+    console.log("");
+    console.log(
+      colors.green(
+        colors.bold(
+          "Usages exemples: https://github.com/conditor-project/co-harvester"
+        )
+      )
+    );
+    console.log("");
+    console.log(
+      colors.green(
+        colors.bold(
+          "More infos about [CONDITOR API] here: https://github.com/conditor-project/api/blob/master/doc/records.md"
+        )
+      )
+    );
+    console.log("");
+  })
+  .parse(process.argv);
+
+// create a new progress bar instance and use shades_classic theme
+const mainProgress = new cliProgress.SingleBar(
+  {},
+  cliProgress.Presets.shades_classic
+);
+
+let query = program.query,
+  api = new URL(query),
+  token = api.searchParams.get("access_token"),
+  criteria = program.criteria,
+  format = program.format,
+  output = program.output,
+  result = [],
+  outputFormat = {
+    object: function(data, criteria, stringify = true) {
+      let result = {};
+      for (var i = 0; i < data.length; i++) {
+        if (typeof result[data[i][criteria]] === "undefined")
+          result[data[i][criteria]] = [];
+        result[data[i][criteria]].push(data[i]);
+      }
+      if (stringify) return JSON.stringify(result);
+      else return result;
+    },
+    array: function(data, criteria, stringify = true) {
+      let hits = outputFormat.object(data, criteria, false),
+        result = [];
+      for (let key in hits) {
+        result.push(data[i]);
+      }
+      if (stringify) return JSON.stringify(result);
+      else return result;
+    },
+    list: function(data, criteria, stringify = true) {
+      let result = Object.keys(outputFormat.object(data, criteria, false));
+      if (stringify) return result.join("\n");
+      else return result;
+    }
+  };
+
+if (typeof format !== "undefined" && typeof outputFormat[format] !== "function")
+  throw new Error(
+    colors.red(
+      "invalid --format. Available values are: object OR array OR list"
+    )
+  );
+
+// Token initialisation
+if (!token) {
+  log("Token not found in --query");
+  if (program.token) {
+    log("Token provided by --token");
+    token = program.token;
+  } else {
+    log("Token not found in --token");
+    if (process.env.CONDITOR_TOKEN) {
+      log("Token provided by ENV $CONDITOR_TOKEN");
+      token = process.env.CONDITOR_TOKEN;
+    } else log("Token not found in ENV $CONDITOR_TOKEN");
+  }
+  api.searchParams.set("access_token", token);
+}
+
+function harvest(target) {
+  log("Request API...");
+  log(target);
+  return fetch(target, {
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(checkStatus)
+    .then(function(res) {
+      log("done.");
+      let scrollId = res.headers.get("scroll-id"),
+        count = +res.headers.get("x-result-count"),
+        total = +res.headers.get("x-total-count");
+      return res
+        .json()
+        .then(function(data) {
+          if (!program.scroll) return console.log(data);
+          else {
+            log("Harvesting API...");
+            if (!program.quiet) mainProgress.start(total, count);
+            result = result.concat(data);
+            return _scroll(scrollId, count).catch(function(err) {
+              throw err;
+            });
+          }
+        })
+        .catch(function(err) {
+          throw err;
+        });
+    })
+    .catch(function(err) {
+      throw err;
+    });
+}
+
+function _scroll(scrollId, previousCount = 0) {
+  let target = buildScrollUrl(scrollId);
+  return fetch(target, {
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(checkStatus)
+    .then(function(res) {
+      let id = res.headers.get("scroll-id"),
+        count = previousCount + +res.headers.get("x-result-count"),
+        total = +res.headers.get("x-total-count");
+      if (!program.quiet) mainProgress.update(count);
+      return res
+        .json()
+        .then(function(data) {
+          result = result.concat(data);
+          if (total > count) return _scroll(id, count);
+          else {
+            if (!program.quiet) mainProgress.stop();
+            log("done.");
+            let data = JSON.stringify(result);
+            if (typeof outputFormat[format] === "function")
+              data = outputFormat[format](result, criteria);
+            return writeResult(output, data);
+          }
+        })
+        .catch(function(err) {
+          throw err;
+        });
+    })
+    .catch(function(err) {
+      throw err;
+    });
+}
+
+function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    let msg = "API respond with status " + res.status;
+    console.log(colors.red(msg));
+    throw new Error(msg);
+  }
+}
+
+function writeResult(output, data, callback) {
+  log("Wrinting data into : " + output + " ...");
+  return fs.writeFile(output, data, "utf8", function(err) {
+    if (err) throw err;
+    log("done.");
+    if (typeof callback === "function") return callback();
+  });
+}
+
+function buildScrollUrl(scrollId) {
+  let result =
+    url.resolve(api.href.split("records")[0], "scroll/" + scrollId) +
+    "?access_token=" +
+    token;
+  return result;
+}
+
+function log(str) {
+  if (!program.quiet) console.log(str);
+}
+
+harvest(api.href);
